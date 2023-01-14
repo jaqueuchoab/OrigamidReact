2d4b394bcbab74804b7546bcea14ff65161fadea
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-01-14 12:11:39 -0300

    Adicionando propriedades

--- src/CardProject/Card.js
+++ src/CardProject/Card.js
@@ -6,12 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Importação do react-bootstrap
 import Card from 'react-bootstrap/Card';
 
-const CardComponent = ({className}) => {
+const CardComponent = ({title, content, className}) => {
      return (
           <Card bg="dark" text="white" className={`mt-4 ${className}`} style={{ maxWidth: '30rem'}}>
-               <Card.Header>Title of Card</Card.Header>
+               <Card.Header>{title || "Title of Card"}</Card.Header>
                <Card.Body>
-                    <Card.Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </Card.Text>
+                    <Card.Text>{content || "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}</Card.Text>
                </Card.Body>
           </Card>
      );
